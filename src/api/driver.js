6fd7101bc36881dd3087ec62e72b923069dc460a
Denison Kolling <denisonkolling@gmail.com>
2024-04-30 08:34:34 -0300

    remove try cath post method

--- src/api/driver.js
+++ src/api/driver.js
@@ -1,12 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const saveDriver = async (driver) => {
-	try {
-		return await axios.post(
-			`${import.meta.env.VITE_API_BASE_URL}/driver`,
-			driver
-		);
-	} catch (e) {
-		throw e;
-	}
+  return await axios.post(
+    `${import.meta.env.VITE_API_BASE_URL}/driver`,
+    driver
+  );
 };
--- src/api/vehicle.js
+++ src/api/vehicle.js
@@ -1,12 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const saveVehicle = async (vehicle) => {
-	try {
-		return await axios.post(
-			`${import.meta.env.VITE_API_BASE_URL}/vehicle`,
-			vehicle
-		);
-	} catch (e) {
-		throw e;
-	}
+  return await axios.post(
+    `${import.meta.env.VITE_API_BASE_URL}/vehicle`,
+    vehicle
+  );
 };
