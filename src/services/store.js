f2f114e587f4c8732a9f570babe8adad0cdc10da
Denison Kolling <denisonkolling@gmail.com>
2024-04-30 11:28:37 -0300

    update zunstad  persist

--- src/services/store.js
+++ src/services/store.js
@@ -1,32 +1,34 @@
 import create from "zustand";
 import { persist } from "zustand/middleware";
 
-const useStore = create(
-  persist(
-    (set) => ({
-      openSidebar: false,
-      toggleOpenSidebar: (value) =>
-        set((state) => ({
-          openSidebar: value || !state.openSidebar,
-        })),
-      selectedSidebarItem: "dashboard",
-      selectSidebarItem: (value) =>
-        set((state) => ({
-          selectedSidebarItem: value,
-        })),
-      colorScheme: "light",
-      toggleColorScheme: (value) =>
-        set((state) => ({
-          colorScheme:
-            value || (state.colorScheme === "dark" ? "light" : "dark"),
-        })),
-    }),
-    {
-      name: "theme",
-      getStorage: () => localStorage,
-      partialize: (state) => ({ colorScheme: state.colorScheme }),
-    }
-  )
+const useStore = persist(
+  (set) => ({
+    openSidebar: false,
+    toggleOpenSidebar: (value) =>
+      set((state) => ({
+        openSidebar: value || !state.openSidebar,
+      })),
+    selectedSidebarItem: "dashboard",
+
+    selectSidebarItem: (value) =>
+      set((state) => ({
+        selectedSidebarItem: value,
+      })),
+    colorScheme: "light",
+
+    toggleColorScheme: (value) =>
+      set((state) => ({
+        colorScheme:
+          value || (state.colorScheme === "dark" ? "light" : "dark"),
+      })),
+  }),
+
+  {
+    name: "theme",
+    getStorage: () => localStorage,
+    partialize: (state) => ({ colorScheme: state.colorScheme }),
+  }
+  
 );
 
-export default useStore;
+export default create(useStore);
